125b6eae8fd390fe4c52e30d4ef546e6bf6bc80b
xxmorwellr <xxmorwellr@163.com>
2024-10-11 23:42:47 +0200

    Update my solution for Lab3

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,26 @@ import { setup, createActor, fromPromise, assign } from "xstate";
 
 const FURHATURI = "127.0.0.1:54321";
 
+async function fhGetUser() {
+  const myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  return fetch(`http://${FURHATURI}/furhat/users`, {
+    method: "POST",
+    headers: myHeaders,
+    body: "",
+  });
+}
+
+async function fhUserTracking() {
+  const myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  return fetch(`http://${FURHATURI}/furhat/attend?user=CLOSEST`, {
+    method: "POST",
+    headers: myHeaders,
+    body: "",
+  });
+}
+
 async function fhSay(text: string) {
   const myHeaders = new Headers();
   myHeaders.append("accept", "application/json");
@@ -13,27 +33,63 @@ async function fhSay(text: string) {
   });
 }
 
-async function newGesture() {
+async function greetingGesture() {
   const myHeaders = new Headers();
   myHeaders.append("accept", "application/json");
   return fetch(`http://${FURHATURI}/furhat/gesture?blocking=false`, {
     method: "POST",
     headers: myHeaders,
     body: JSON.stringify({
-      name: "newGesture",
+      name: "Greeting",
       frames: [
         {
-          time: [], //ADD THE TIME FRAME OF YOUR LIKING
+          time: [0, 0.4], //ADD THE TIME FRAME OF YOUR LIKING
           persist: true,
           params: {
             //ADD PARAMETERS HERE IN ORDER TO CREATE A GESTURE
+            "SURPRISE":1,
+            "NECK_ROLL":0.7,
+            "SMILE_OPEN":0.4,
+            "SMILE_CLOSED":0.7
           },
         },
         {
-          time: [], //ADD TIME FRAME IN WHICH YOUR GESTURE RESETS
-          persist: true,
+          time: [0.7], //ADD TIME FRAME IN WHICH YOUR GESTURE RESETS
+          persist: false, //optional
           params: {
             reset: true,
+          },
+        },
+        //ADD MORE TIME FRAMES IF YOUR GESTURE REQUIRES THEM
+      ],
+      class: "furhatos.gestures.Gesture",
+    }),
+  });
+}
+
+async function nodGesture() {
+  const myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  return fetch(`http://${FURHATURI}/furhat/gesture?blocking=false`, {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify({
+      name: "Nod",
+      frames: [
+        {
+          time: [0.17, 1.0, 6.0], // time range
+          persist: true,
+          params: {
+            "LOOK_DOWN": 0.7,
+            "NECK_PAN": -12.0,
+            "NECK_TILT": -25.0
+          }
+        },
+        {
+          time: [0.7], //ADD TIME FRAME IN WHICH YOUR GESTURE RESETS
+          persist: false, //optional
+          params: {
+              reset: true,
           },
         },
         //ADD MORE TIME FRAMES IF YOUR GESTURE REQUIRES THEM
@@ -72,23 +128,37 @@ async function fhListen() {
 const dmMachine = setup({
   actors: {
     fhHello: fromPromise<any, null>(async () => {
-      return fhSay("Hi");
+      return Promise.all([
+        fhSay("Hi"), 
+        greetingGesture()
+      ])
     }),
     fhL: fromPromise<any, null>(async () => {
      return fhListen();
-   }),
+    }),
+    fhGetUser: fromPromise<any, null>(async () => {
+      return fhGetUser();
+    }),
+    fhUserTracker: fromPromise<any, null>(async () => {
+      return fhUserTracking();
+    }),
+    fhSpeak: fromPromise<any, {message: string}>(async ({input}) => {
+      return Promise.all([
+        fhSay(input.message),
+      ])
+    }),
   },
 }).createMachine({
   id: "root",
   initial: "Start",
   states: {
-    Start: { after: { 1000: "Next" } },
-    Next: {
+    Start: { after: { 1000: "GetUser" } },
+    GetUser: {
       invoke: {
-        src: "fhHello",
+        src: "fhGetUser",
         input: null,
         onDone: {
-          target: "Recognised",
+          target: "UserTracking",
           actions: ({ event }) => console.log(event.output),
         },
         onError: {
@@ -97,8 +167,65 @@ const dmMachine = setup({
         },
       },
     },
-    Recognised: {},
-    Fail: {},
+    UserTracking: {
+      invoke: {
+        src: "fhUserTracker",
+        input: null,
+        onDone: {
+          target: "Greeting",
+          actions: ({ event }) => console.log(event.output),
+        },
+        onError: {
+          target: "Fail",
+          actions: ({ event }) => console.error(event),
+        },
+      },
+    },
+   Greeting: {
+      invoke: {
+        src: "fhHello",
+        input: null,
+        onDone: {
+          target: "Listen1",
+          actions: ({ event }) => console.log(event.output),
+        },
+        onError: {
+          target: "Fail",
+          actions: ({ event }) => console.error(event),
+        },
+      },
+    },
+    Listen1: {
+      invoke: {
+        src: "fhL",
+        input: null,
+        onDone: {
+          target: "Confused",
+          actions: [
+            ({ event }) => console.log(event.output), 
+            assign({ lastResult: ({ event }) => event.output,}),
+        ]},
+        onError: {
+          target: "Fail",
+          actions: ({ event }) => console.error(event),
+        },
+      },
+    },
+    Confused: {
+      invoke: {
+        src : "fhSpeak",
+        input: { message: "Ok! I would try" },
+        onDone: {
+          target:"Speak",
+          actions: ({ event }) => console.log(event.output)
+        },
+        onError: {
+          target: "Fail",
+          actions: ({ event }) => console.error(event)
+      }
+    },
+  },
+  Fail: {},
   },
 });
 
